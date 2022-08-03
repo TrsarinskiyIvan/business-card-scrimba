b5b413b945cbae15aa10df8e04e3f3a89d3da1fe
Ivan Tsarinskiy <itsarinskiy@gmail.com>
2022-08-03 16:01:23 +0300

    finish 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,19 @@
 export default function Footer() {
     return (
         <footer className="social">
-            <a href="#">
+            <a href="/">
                 <img src="./facebook.png" alt="facebook" />
             </a>
-            <a href="">
+            <a href="/">
                 <img src="./instagram.png" alt="instagram" />
             </a>
-            <a href="">
+            <a href="/">
                 <img src="./twiter.png" alt="twiter" />
             </a>
-            <a href="">
+            <a href="/">
                 <img src="./in.png" alt="linked id" />
             </a>
-            <a href="">
+            <a href="/">
                 <img src="./github.png" alt="git hub" />
             </a>
         </footer>
